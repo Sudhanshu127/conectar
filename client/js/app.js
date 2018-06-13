c8267fb40bdeb979098a297f6b4e346b76b0bc60
Sudhanshu127 <subansal@iitk.ac.in>
2018-06-13 13:58:23 +0000

    Private chat started working

--- client/js/app.js
+++ client/js/app.js
@@ -2,11 +2,18 @@ var app=angular.module('myApp',[]);
 
 app.controller('mainController',['$scope',function($scope){
  var socket = io.connect();
- $scope.messages="1";
- $scope.send = function(){
-  socket.emit('chat message', $scope.message);
-  $scope.message="";
+ var roome="Sudhanshu";
+ $scope.submit =function(response){
+ 	roome=response;
+ 	$scope.roome=roome;
+ 	socket.emit('join',roome);
  }
+
+ $scope.send = function(){
+	var msg={msg:$scope.message , room:roome};
+	socket.emit('chat message', msg);
+	$scope.message="";
+}
  socket.on('chat message', function(msg){
   var li=document.createElement("li");
   li.appendChild(document.createTextNode(msg));
--- server/server.js
+++ server/server.js
@@ -6,11 +6,15 @@ app.use(express.static("./../client"));
 app.get('/', function(req, res){
   res.sendFile('./../client/index.html');
 });
-
+var room;
 io.on('connection', function(socket){
   console.log('user connected');
+  socket.on('join',function(roome){
+  	socket.join(roome);
+  	room=roome;
+  })
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.sockets.in(room).emit('chat message', msg.msg);
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
